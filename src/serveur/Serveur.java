5ea77f4c20eaeac82bb50ca23648b8703f8687fd
Clement Audry <audry@polytech.unice.frgit add http///audry@atlassian.polytech.unice.fr:7990/scm/OONPH/public.git NP4Hgit config --global user.email audry@polytech.unice.fr>
2015-04-21 10:59:17 +0200

    push initial

---
+++ src/serveur/Serveur.java
@@ -0,0 +1,17 @@
+package serveur;
+
+public class Serveur {
+
+	public Serveur() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
